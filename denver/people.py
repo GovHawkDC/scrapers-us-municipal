dbd7af6c613c2bec17a492fa0cc98cf02361fdb6
Tim Showers <showerst@gmail.com>
2019-02-14 15:13:05 -0500

    Only emit serving councilmembers and scrape districts

--- denver/people.py
+++ denver/people.py
@@ -2,6 +2,7 @@ import collections
 import datetime
 import re
 import pytz
+import lxml
 
 from legistar.people import LegistarAPIPersonScraper, LegistarPersonScraper
 from pupa.scrape import Person, Organization, Scraper, Membership
@@ -15,6 +16,7 @@ class DenverPersonScraper(LegistarAPIPersonScraper, Scraper):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.people_districts = {}
         self.all_people = {}
         self.all_bodies = {}
 
@@ -24,8 +26,26 @@ class DenverPersonScraper(LegistarAPIPersonScraper, Scraper):
             'City Council': 'legislature',
             'Special Issues Marijuana': 'committee',
             'Special Issues: Green Roof Initiative': 'committee',
-            'Administration': 'committee'
+            'Administration': 'committee',
+            'Mayor': 'executive',
         }
+    def scrape(self):
+        self.scrape_districts()
+        yield from self.scrape_bodies()
+        yield from self.scrape_people()
+        yield from self.scrape_body_membership()
+
+    def scrape_districts(self):
+        districts_url = 'https://denver.legistar.com/DepartmentDetail.aspx?ID=33319&GUID=2B4CB4CB-A523-45F4-8FCF-074E8041F1B9&Search='
+        page = self.get(districts_url).text
+        page = lxml.html.fromstring(page)
+
+        for row in page.xpath('//table[@id="ctl00_ContentPlaceHolder1_gridPeople_ctl00"]/tbody/tr'):
+            # careful, this puts FONT tags in the source that chrome doesn't show
+            name = row.xpath('td[1]/font/a/font/text()')[0]
+            district = row.xpath('td[2]/font/em/text()')[0]
+            self.people_districts[name] = district
+
 
     def scrape_bodies(self):
         for body in self.bodies():
@@ -49,6 +69,7 @@ class DenverPersonScraper(LegistarAPIPersonScraper, Scraper):
             org.add_source(self.COMMITTEE_LIST)
 
             self.all_bodies[body['BodyId']] = org
+
             yield org
 
     def scrape_body_membership(self):
@@ -87,9 +108,15 @@ class DenverPersonScraper(LegistarAPIPersonScraper, Scraper):
             if row['PersonActiveFlag'] == 0:
                 continue
 
+            if row['PersonFullName'] not in self.people_districts:
+                self.warning("Unable to find {}".format(row['PersonFullName']))
+                continue
+
             person = Person(
                 name=row['PersonFullName'],
                 primary_org="legislature",
+                role="councilmember",
+                district=self.people_districts[row['PersonFullName']],
             )
 
             person.add_source('https://www.denvergov.org/content/denvergov/en/denver-city-council/council-members.html', note="Council Page")
@@ -110,9 +137,3 @@ class DenverPersonScraper(LegistarAPIPersonScraper, Scraper):
             isodate = datetime.datetime.strptime(isodate, '%Y-%m-%dT%H:%M:%S')
         isodate = pytz.timezone(self.TIMEZONE).localize(isodate)
         return isodate
-
-    def scrape(self):
-
-        yield from self.scrape_bodies()
-        yield from self.scrape_people()
-        yield from self.scrape_body_membership()
