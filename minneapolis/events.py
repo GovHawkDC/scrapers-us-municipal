5094e853937a9f2e22f857ea89102bb91bab1dab
Tim Showers <showerst@gmail.com>
2020-04-28 23:08:27 -0400

    MPLS: working date ranges and bill linkages

--- minneapolis/events.py
+++ minneapolis/events.py
@@ -1,61 +1,85 @@
 from pupa.scrape import Scraper
 from pupa.scrape import Event
 import json
-import datetime
-import lxml
+import datetime as dt
+import lxml.html
 import pprint
 import pytz
 
+
 class MinneapolisEventScraper(Scraper):
-    TIMEZONE = pytz.timezone('America/Chicago')
-    AGENDA_URL = 'https://lims.minneapolismn.gov/MarkedAgenda/{com_abbr}/{id}'
+    TIMEZONE = pytz.timezone("America/Chicago")
+    AGENDA_URL = "https://lims.minneapolismn.gov/MarkedAgenda/{com_abbr}/{id}"
 
     def scrape(self, start_date=None, end_date=None):
-        # needs to be implemented
-        # https://lims.minneapolismn.gov/Calendar/GetCalenderList?fromDate=Mar%2029,%202020&toDate=May%203,%202020&meetingType=2&committeeId=null&pageCount=1000&offsetStart=0&abbreviation=undefined&keywords=
-        events_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList?fromDate=Jan%2001,%202020&toDate=May%203,%202020&meetingType=1&committeeId=null&pageCount=1000&offsetStart=0&abbreviation=undefined&keywords="
+
+        if start_date is None:
+            start_date = dt.datetime.today() - dt.timedelta(days=30)
+            end_date = dt.datetime.today() + dt.timedelta(days=30)
+        else:
+            start_date = dt.datetime.strptime(start_date,"%Y-%m-%d")
+            end_date = dt.datetime.strptime(end_date,"%Y-%m-%d")
+
+        date_input_format = "%b %d, %Y"
+
+        events_url = (
+            "https://lims.minneapolismn.gov/Calendar/GetCalenderList?fromDate="
+            "{}&toDate={}&meetingType=1&committeeId=null&pageCount=1000&offsetStart=0"
+            "&abbreviation=undefined&keywords="
+        ).format(start_date.strftime(date_input_format), end_date.strftime(date_input_format))
+        
         req = self.get(events_url)
         rows = json.loads(req.content)
         for row in rows:
             yield from self.scrape_event(row)
 
     def scrape_event(self, row):
-        location = row['Address']
+        location = row["Address"]
 
         if not location:
-            location = 'Not Provided'
+            location = "Not Provided"
 
         # 2020-04-29T13:30:00
-        event_date_str = row['MeetingTime']
-        event_date = datetime.datetime.strptime(event_date_str, "%Y-%m-%dT%H:%M:%S")
+        event_date_str = row["MeetingTime"]
+        event_date = dt.datetime.strptime(event_date_str, "%Y-%m-%dT%H:%M:%S")
         event_date = self.TIMEZONE.localize(event_date)
 
-        description = str(row['Description'])
+        description = str(row["Description"])
 
-        status = ''
-        if event_date > self.TIMEZONE.localize(datetime.datetime.today()):
-            status = 'tentative'
+        status = ""
+        if event_date > self.TIMEZONE.localize(dt.datetime.today()):
+            status = "tentative"
         else:
-            status = 'passed'
+            status = "passed"
 
-        if 'cancellation' in description.lower() or 'cancelled' in description.lower():
-            status = 'cancelled'
+        if "cancellation" in description.lower() or "cancelled" in description.lower():
+            status = "cancelled"
 
         event = Event(
-            name=row['CommitteeName'],
+            name=row["CommitteeName"],
             start_date=event_date,
             location_name=location,
             status=status,
+            description=description,
         )
 
-        if row['CommitteeName']:
-            event.add_participant(name=row['CommitteeName'],
-                    type="organization")
+        if row["CommitteeName"]:
+            event.add_participant(name=row["CommitteeName"], type="organization")
 
-        if row['MarkedAgendaPublished']:
-            agenda_url = self.AGENDA_URL.format(com_abbr=row['Abbreviation'], id=row['AgendaId'])
-            print(agenda_url)
+        if row["MarkedAgendaPublished"]:
+            agenda_url = self.AGENDA_URL.format(
+                com_abbr=row["Abbreviation"], id=row["AgendaId"]
+            )
+            event.add_document("Agenda", agenda_url, media_type="text/html")
 
-        event.add_source('https://lims.minneapolismn.gov/Calendar/all/monthly')
-        print( event)
+            agenda_req = self.get(agenda_url).content
+            agenda_page = lxml.html.fromstring(agenda_req)
+            agenda_page.make_links_absolute("https://lims.minneapolismn.gov/")
+
+            agenda_item = event.add_agenda_item("Bills")
+            for bill_link in agenda_page.xpath('//a[contains(@href, "/File/")]'):
+                bill_id = bill_link.xpath("text()")[0].strip()
+                bill_id = bill_id.replace("(", "").replace(")", "")
+                agenda_item.add_bill(bill_id)
+        event.add_source("https://lims.minneapolismn.gov/Calendar/all/monthly")
         yield event
